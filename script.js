27eaac7898062006e7d066ed284d751e3190921a
HrvojeD <hrvoje.dondivic@gmail.com>
2020-01-06 20:55:40 +0100

    added logic and style

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-const columns = 40;
-const rows = 40;
+const columns = 30;
+const rows = 30;
+blackRadio.checked = true;
 
 makeGrid(columns, rows);
 
@@ -11,6 +12,32 @@ function makeGrid(columns, rows) {
     for (var i=0; i<size; i++) {
         const gridItem = document.createElement("div");
         gridItem.setAttribute("class", "gridCell");
+        gridItem.setAttribute("onmouseover", "onMouseOver(this)")
         container.appendChild(gridItem);
     }
+}
+
+function onMouseOver(gridItem) {
+    const blackRadio = document.getElementById("blackRadio");
+    const rainbowRadio = document.getElementById("rainbowRadio");
+
+    if (blackRadio.checked) {
+        gridItem.style.backgroundColor = "black";
+    }
+
+    if (rainbowRadio.checked) {
+        const r = Math.floor(Math.random()*256);
+        const g = Math.floor(Math.random()*256);
+        const b = Math.floor(Math.random()*256);
+
+        gridItem.style.backgroundColor = "rgb(" + r + "," + g + "," + b + ")";
+    }
+    
+}
+
+function resetGrid() {
+    const gridCells = document.querySelectorAll(".gridCell");
+    gridCells.forEach(element => {
+            element.style.backgroundColor = "white"
+   });
 }
